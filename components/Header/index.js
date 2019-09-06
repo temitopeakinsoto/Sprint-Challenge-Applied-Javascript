b9396d75cd9866ab4012db09bcf0ae0cdc91e0e2
Temitope Akinsoto <takinsoto@gmail.com>
2019-09-06 08:18:43 +0100

    feat: Create a header component maker function
    In this commit, I created a component maker function called Header(). what this
    function does is to create a new header component composed of div and within
    this div are nested one h1 and 2 span elements. upon invokation of this funct-
    ion, the returned component is then appended to the document's div of class
    .header-container

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    const divElement = document.createElement('div');
+    divElement.classList.add('header');
+
+    const firstSpanElement = document.createElement('span');
+    firstSpanElement.classList.add('date');
+    firstSpanElement.textContent = "SMARCH 28, 2019";
+
+    const h1Element = document.createElement('h1');
+    h1Element.textContent = "Lambda Times";
+
+    const lastSpanElement = document.createElement('span');
+    lastSpanElement.classList.add('temp');
+    lastSpanElement.textContent = "98";
+
+    divElement.appendChild(firstSpanElement);
+    divElement.appendChild(h1Element);
+    divElement.appendChild(lastSpanElement);
+
+    return divElement;
+}
+
+const header = Header();
+document.querySelector('.header-container').appendChild(header);
