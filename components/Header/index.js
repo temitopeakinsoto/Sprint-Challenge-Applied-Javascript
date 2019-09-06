e4d8dec21dd0eb1490539ae61111b4ccc5dd5e8e
Temitope Akinsoto <takinsoto@gmail.com>
2019-09-06 08:33:40 +0100

    REFACTOR: Modify Header component-maker function
    In this commit, I modified the header component-maker function to take a single
    object parameter. properties of this object can then be used to populate the
    newly created component. this was done to allow for re-usability of the component
    maker function

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,29 +9,33 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {
+const headerInfo = {
+    date: "MARCH 28, 2019",
+    heading: "Lambda Times",
+    temp: "98"
+}
+
+function Header(headerInfo) {
     const divElement = document.createElement('div');
     divElement.classList.add('header');
 
     const firstSpanElement = document.createElement('span');
     firstSpanElement.classList.add('date');
-    firstSpanElement.textContent = "SMARCH 28, 2019";
+    firstSpanElement.textContent = headerInfo.date;
 
     const h1Element = document.createElement('h1');
-    h1Element.textContent = "Lambda Times";
+    h1Element.textContent = headerInfo.heading;
 
     const lastSpanElement = document.createElement('span');
     lastSpanElement.classList.add('temp');
-    lastSpanElement.textContent = "98";
+    lastSpanElement.textContent = headerInfo.temp;
 
     divElement.appendChild(firstSpanElement);
     divElement.appendChild(h1Element);
     divElement.appendChild(lastSpanElement);
     
-    document.querySelector('.header-container').appendChild(divElement);
-    //return divElement;
-    
+    document.querySelector('.header-container').appendChild(divElement);    
 }
 
-const header = Header();
+const header = Header(headerInfo);
 
